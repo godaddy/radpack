9f29bd73b8c1546b729dea03a80099b78833eb86
gingur <troy.rhinehart@gmail.com>
2021-07-06 14:41:45 -0700

    add default timeout support, update constants

--- packages/core/src/server/fetch.js
+++ packages/core/src/server/fetch.js
@@ -1,16 +1,17 @@
 import fetch from 'cross-fetch';
 import fetchFile from './fetch-file';
+import { DEFAULT_DELAY, DEFAULT_TIMEOUT } from './constants';
 
 const sleep = ms => new Promise(resolve => ms > 0 ? setTimeout(resolve, ms) : resolve());
 
-const smartFetch = (url, { delay = 5000, retries = 0, ...options } = {}) => {
+const smartFetch = (url, { delay = DEFAULT_DELAY, timeout = DEFAULT_TIMEOUT, retries = 0, ...options } = {}) => {
   if (url.startsWith('file:')) {
     return fetchFile(url);
   }
   const retry = retries-- > 0
     ? () => sleep(delay).then(() => smartFetch(url, { ...options, delay, retries }))
     : false;
-  return fetch(url, options).then(res => !res.ok && res.status !== 304 && retry ? retry() : res, err => {
+  return fetch(url, { ...options, timeout }).then(res => !res.ok && res.status !== 304 && retry ? retry() : res, err => {
     if (retry) {
       return retry();
     }
