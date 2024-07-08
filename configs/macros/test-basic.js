30fb7037e9c336b570824dc134bbc3f15cbc675c
trhinehart-godaddy <trhinehart@godaddy.com>
2024-07-08 13:41:15 -0700

    node@20 + npm fixes + workspaces

--- configs/macros/test-basic.js
+++ configs/macros/test-basic.js
@@ -5,7 +5,7 @@ import * as module from '../../examples/libs/basic/src/module';
 export default async function (t, dir) {
   const results = await t.context.run(dir);
   t.is(results.array.length, 3, results.raw);
-  t.like(results.object['basic.entry'], { exports: entry });
-  t.like(results.object['basic.chunk'], { exports: chunk });
-  t.like(results.object['basic.module'], { exports: module });
+  t.deepEqual(results.object['basic.entry'].exports, entry);
+  t.deepEqual(results.object['basic.chunk'].exports, chunk);
+  t.deepEqual(results.object['basic.module'].exports, module);
 }
--- configs/macros/test-context.js
+++ configs/macros/test-context.js
@@ -4,6 +4,6 @@ import * as b from '../../examples/libs/context/src/b/module';
 export default async function (t, dir) {
   const results = await t.context.run(t.context.resolve(dir));
   t.is(results.array.length, 2, results.raw);
-  t.like(results.object['context-a.module'], { exports: a });
-  t.like(results.object['context-b.module'], { exports: b });
+  t.deepEqual(results.object['context-a.module'].exports, a);
+  t.deepEqual(results.object['context-b.module'].exports, b);
 }
